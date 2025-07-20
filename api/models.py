3798cb0df8ef8ec1b25a77709e757c79631a23bb
nliou <nliou@students.cs.ubc.ca>
2025-07-19 21:51:04 -0700

    Initial Commit

---
+++ api/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class StudySpot(models.Model):
+    name = models.CharField(max_length=50)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    
+    def __str__(self):
+        return self.name
+    
+class Criteria(models.Model):
+    attribute = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.attribute
+    
+class SpotCriteria(models.Model):
+    studySpot = models.ForeignKey(StudySpot, on_delete=models.CASCADE)
+    criteria = models.ForeignKey(Criteria, on_delete=models.CASCADE)
+    
