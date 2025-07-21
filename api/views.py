79f933967170c646f2dcf4fe1ce818237b4f9312
nliou <nliou@students.cs.ubc.ca>
2025-07-20 23:38:08 -0700

    add create and delete views

--- api/views.py
+++ api/views.py
@@ -2,22 +2,23 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import StudySpot, Criteria, SpotCriteria
+from . import serializers
 
 
 # Create your views here.
-class ListSpots(APIView):
+class listSpots(APIView):
     def get(self, request):
         spots = StudySpot.objects.all()
-        data = [{"name": spot.name, "latitude": spot.latitude, "longitude": spot.longitude} for spot in spots]
-        return Response(data)
+        serializer = serializers.StudySpotSerializer(spots, many=True)
+        return Response(serializer.data)
     
-class ListCriteria(APIView):
+class listCriteria(APIView):
     def get(self, request):
         criteria = Criteria.objects.all()
-        data = [{"attribute": crit.attribute} for crit in criteria]
-        return Response(data)
+        serializer = serializers.CriteriaSerializer(criteria, many=True)
+        return Response(serializer.data)
     
-class ListSpotCriteria(APIView):
+class listSpotCriteria(APIView):
     def get(self, request):
         spot_criteria = SpotCriteria.objects.all()
         data = []
@@ -32,7 +33,7 @@ class ListSpotCriteria(APIView):
             })
         return Response(data)
     
-class ListSpotsWithCriteria(APIView):
+class listCriteriaInSpot(APIView):
     def get(self, request):
         spots = StudySpot.objects.all()
         data = []
@@ -46,3 +47,58 @@ class ListSpotsWithCriteria(APIView):
                 "criteria": criteria_data
             })
         return Response(data)
+    
+class listSpotsWithCriteria(APIView):
+    def get(self, request):
+        criteria = Criteria.objects.all()
+        data = []
+        for c in criteria:
+            spotList = SpotCriteria.objects.filter(criteria=c)
+            spotData = [{"attribute": sc.criteria.attribute} for sc in spotList]
+            data.append({
+                "attribute": c.attribute,
+                "spots": spotData
+            })
+        return Response(data)
+    
+class addSpot(APIView):
+    def get(self, request):
+        return Response({"message": "This endpoint only accepts POST."})
+    
+    def post(self, request):
+        serializer = serializers.StudySpotSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+class addCriteria(APIView):
+    def get(self, request):
+        return Response({"message": "This endpoint only accepts POST."})
+    
+    def post(self, request):
+        serializer = serializers.CriteriaSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+    
+class addSpotCriteria(APIView):
+    def get(self, request):
+        return Response({"message": "This endpoint only accepts POST."})
+    
+    def post(self, request):
+        serializer = serializers.SpotCriteriaSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+    
+class deleteSpot(APIView):
+    def delete(self, request, pk):
+        try:
+            spot = StudySpot.objects.get(pk=pk)
+            spot.delete()
+            return Response(status=204)
+        except StudySpot.DoesNotExist:
+            return Response({"error": "StudySpot not found"}, status=404)
