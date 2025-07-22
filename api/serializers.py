0aacf6c075ce62848383047c17bec9a73ac949d7
cannonfodderalt <cannonfodderalt@gmail.com>
2025-07-22 12:22:54 -0700

    change to modelserializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,32 +1,26 @@
 from rest_framework import serializers
 from .models import StudySpot, Criteria, SpotCriteria
 
-class StudySpotSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=50)
-    latitude = serializers.FloatField()
-    longitude = serializers.FloatField()
+class StudySpotSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StudySpot
+        fields = '__all__'
     
-    def create(self, validated_data):
-        return StudySpot.objects.create(**validated_data)
-    
-class CriteriaSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    attribute = serializers.CharField(max_length=50)
-    
-    def create(self, validated_data):
-        return Criteria.objects.create(**validated_data)
+class CriteriaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Criteria
+        fields = '__all__'
     
     
-class SpotCriteriaSerializer(serializers.Serializer):
-    studySpot = StudySpotSerializer()
-    criteria = CriteriaSerializer()
-    
-    def create(self, validated_data):
-        study_spot_data = validated_data.pop('studySpot')
-        criteria_data = validated_data.pop('criteria')
+class SpotCriteriaSerializer(serializers.ModelSerializer):
+    studySpot = StudySpotSerializer(read_only=True)
+    criteria = CriteriaSerializer(read_only=True)
+
+    class Meta:
+        model = SpotCriteria
+        fields = '__all__'
         
-        study_spot = StudySpot.objects.create(**study_spot_data)
-        criteria = Criteria.objects.create(**criteria_data)
-        
-        return SpotCriteria.objects.create(studySpot=study_spot, criteria=criteria)
+class SpotCriteriaCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SpotCriteria
+        fields = ['studySpot', 'criteria']
