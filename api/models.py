1452c119fa68420a5bea7b80bfa96e342a93dee4
cannonfodderalt <cannonfodderalt@gmail.com>
2025-08-19 19:58:10 -0700

    deploy django to azure app service

--- api/models.py
+++ api/models.py
@@ -1,14 +1,21 @@
 from django.db import models
+from .utils import generate_private_image_url
 
 # Create your models here.
 class StudySpot(models.Model):
     name = models.CharField(max_length=50)
     latitude = models.FloatField()
     longitude = models.FloatField()
-    image_url = models.CharField(max_length=100, default='pikachu.jpg')
     
     def __str__(self):
         return self.name
+    
+    def get_image_urls(self):
+        images = SpotImages.objects.filter(studySpot=self)
+        return [
+            generate_private_image_url(img.image_url)
+            for img in images
+        ]
     
 class Criteria(models.Model):
     attribute = models.CharField(max_length=50)
@@ -22,3 +29,10 @@ class SpotCriteria(models.Model):
     
     def __str__(self):
         return str(self.studySpot) + " + " + str(self.criteria)
+    
+class SpotImages(models.Model):
+    studySpot = models.ForeignKey(StudySpot, on_delete=models.CASCADE, related_name='images')
+    image_url = models.URLField(max_length=200)
+    
+    def __str__(self):
+        return f"Image for {self.studySpot.name} at {self.image_url}"
