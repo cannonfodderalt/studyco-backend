b14c8718b373d2b64ab4e5ec3e9e34f8da738a9a
cannonfodderalt <cannonfodderalt@gmail.com>
2025-09-05 12:28:31 -0700

    add 3 hour time limit for review

--- api/views.py
+++ api/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from .models import StudySpot, Criteria, SpotCriteria, Score, BusynessLevel
 from . import serializers
 from django_filters.rest_framework import DjangoFilterBackend
+from django.utils import timezone
+from datetime import timedelta
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 
 # StudySpot ViewSet
@@ -41,3 +43,38 @@ class ScoreViewSet(viewsets.ModelViewSet):
 class BusynessLevelViewSet(viewsets.ModelViewSet):
     queryset = BusynessLevel.objects.all()
     serializer_class = serializers.BusynessLevelSerializer
+
+    def get_queryset(self):
+        cutoff = timezone.now() - timedelta(hours=3)
+        # Clean up old entries (simple approach)
+        BusynessLevel.objects.filter(submissionDate__lt=cutoff).delete()
+        return super().get_queryset().filter(submissionDate__gte=cutoff)
+
+    @action(detail=True, methods=["get"], url_path="spot")
+    def spot_busyness(self, request, pk=None):
+        cutoff = timezone.now() - timedelta(hours=3)
+        qs = BusynessLevel.objects.filter(
+            studySpot_id=pk,
+            submissionDate__gte=cutoff
+        ).order_by("-submissionDate")
+
+        last_three = qs[:3]
+
+        if not last_three:
+            return Response({
+                "average": None,
+                "last_review": None,
+                "reviews": []
+            })
+
+        scores = [b.score.rank for b in last_three]
+        avg_score = sum(scores) / len(scores)
+        last_review = last_three[0].submissionDate
+
+        serializer = self.get_serializer(last_three, many=True)
+
+        return Response({
+            "average": round(avg_score, 2),  # rounded to 2 decimals
+            "last_review": last_review,
+            "reviews": serializer.data
+        })
