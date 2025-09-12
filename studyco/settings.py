e1724752536fec7a7dec09856c0c4ec13c7a8c78
cannonfodderalt <cannonfodderalt@gmail.com>
2025-09-11 19:22:54 -0700

    update requirements.txt

--- studyco/settings.py
+++ studyco/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h1s9r@!r12xyu+6509_)!yk1r=*wa*g(i-+5vzx*$%+@asigqs'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -32,7 +31,8 @@ ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
     "192.168.68.161",
-    '172.20.10.2'
+    '172.20.10.2',
+    "studyco-ghf9anhyf7g0fcgb.westus3-01.azurewebsites.net"
 ]
 
 
